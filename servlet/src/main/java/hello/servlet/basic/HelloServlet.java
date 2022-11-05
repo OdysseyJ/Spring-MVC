47066d0bdc914faa20d725febe0738b409211258
seongwoon jeong <ggogumalove@gmail.com>
2022-11-05 22:00:03 +0900

    11/5{

--- servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -2,14 +2,15 @@ package hello.servlet.basic;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
-public class HelloServlet {
+public class HelloServlet extends HttpServlet {
 
+    @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException{
-
         System.out.println("request = " + request + ", response = " + response);
 
     }
---
+++ servlet/src/main/java/hello/servlet/basic/RequestParamServlet.java
@@ -0,0 +1,25 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("RequestParamServlet.service");
+        req.getParameterNames().asIterator().forEachRemaining(paramName -> System.out.println(paramName + "=" + req.getParameter(paramName)));
+        System.out.println();
+        System.out.println("단일 파라미터");
+
+        String username = req.getParameter("username");
+        String age = req.getParameter("age");
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+    }
+}
