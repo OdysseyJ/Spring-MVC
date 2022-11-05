bf6ddcfde31d3f6581754cf054f253d65790d137
seongwoon jeong <ggogumalove@gmail.com>
2022-11-05 21:36:45 +0900

    11/5

---
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,16 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet {
+
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException{
+
+        System.out.println("request = " + request + ", response = " + response);
+
+    }
+}
