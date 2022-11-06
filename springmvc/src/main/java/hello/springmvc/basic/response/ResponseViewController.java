f73725db877b255dabdc2b6d182041c9988190dd
seongwoon jeong <ggogumalove@gmail.com>
2022-11-07 08:36:31 +0900

    11/7

---
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,28 @@
+package hello.springmvc.basic.response;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+    @RequestMapping("/response-view-v1")
+    public ModelAndView reponseViewV1(){
+        ModelAndView mav = new ModelAndView("response/hello").addObject("data", "hello!");
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String reponseViewV2(Model model){
+        model.addAttribute("data", "hello!");
+        return "response/hello";
+    }
+
+    // 추천하지 않음
+    @RequestMapping("/response/hello")
+    public void reponseViewV3(Model model){
+        model.addAttribute("data", "hello!");
+    }
+}
