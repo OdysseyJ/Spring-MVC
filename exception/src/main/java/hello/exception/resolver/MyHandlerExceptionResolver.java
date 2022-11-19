23c54610fc81ae5c2d462f698221934b0fd93758
seongwoon jeong <ggogumalove@gmail.com>
2022-11-19 23:14:56 +0900

    study 11/19:

---
+++ exception/src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
@@ -0,0 +1,28 @@
+package hello.exception.resolver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        try {
+            if (ex instanceof IllegalArgumentException) {
+                log.info("500 to 400");
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            }
+        } catch (IOException e) {
+            log.error("error");
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+}
